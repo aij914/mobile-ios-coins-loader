55062407d2ddbc3e6923e739fc0d7970e1a23109
Kiu Ai <aij914@gmail.com>
2021-01-18 15:09:08 +0800

    setup project.

---
+++ mobile-ios-coins-loader/mobile_ios_coins_loaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_ios_coins_loaderApp.swift
+//  mobile-ios-coins-loader
+//
+//  Created by Kiu Ai on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct mobile_ios_coins_loaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
